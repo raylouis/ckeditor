299da9faba2fdfccfa684526396908be65cabcbc
Nic Wortel <nic-nld@live.nl>
2014-03-22 10:09:08 +0100

    Added Dutch translation

---
+++ i18n/nl-message.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * CKEditor plugin
+ *
+ * This plugin adds CKEditor as a filter in Wolf CMS.
+ *
+ * @package     Plugins
+ * @subpackage  ckeditor
+ *
+ * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @copyright   Nic Wortel, 2013
+ * @version     0.1.0
+ */
+
+return array(
+    'CKEditor' => 'CKEditor',
+    'Adds CKEditor as a WYSIWYG filter to Wolf CMS.' => 'Voegt CKEditor als WYSIWYG filter toe aan Wolf CMS.',
+);
